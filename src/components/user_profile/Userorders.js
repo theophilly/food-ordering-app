c71a228a3f09b83fe775fd813f67190cdc006665
theodasa <theophilly20@gmail.com>
2021-10-16 15:42:15 +0100

    adding orders data

--- src/components/user_profile/Userorders.js
+++ src/components/user_profile/Userorders.js
@@ -25,49 +25,110 @@ import {
 import { DataGrid } from '@material-ui/data-grid';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
+  { field: 'id', headerName: 'ID', width: 40 },
   {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
+    field: 'food_name',
+    headerName: 'Ordered Item',
+    width: 180,
+    renderCell: (params) => (
+      <Box display="flex" alignItems="center">
+        <img
+          style={{
+            width: '40px',
+            height: '40px',
+            objectFit: 'contain',
+            borderRadius: '5px',
+          }}
+          src={params.row.image}
+        />
+        <Box
+          height="30px"
+          display="flex"
+          flexDirection="column"
+          justifyContent=" center"
+          marginLeft="5px"
+        >
+          <Typography
+            style={{
+              fontFamily: 'Mulish',
+              fontSize: '0.9rem',
+              fontWeight: '700',
+            }}
+          >
+            {params.row.food_name}
+          </Typography>
+          <Typography variant="caption">{params.row.category}</Typography>
+        </Box>
+      </Box>
+    ),
+  },
+  {
+    field: 'date',
+    headerName: 'Date',
+    width: 200,
     editable: true,
   },
   {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
+    field: 'payment_method',
+    headerName: 'Payment Method',
     width: 110,
     editable: true,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
+    field: 'status',
+    headerName: 'Status',
     description: 'This column has a value getter and is not sortable.',
     sortable: false,
     width: 160,
-    valueGetter: (params) =>
-      `${params.getValue(params.id, 'firstName') || ''} ${
-        params.getValue(params.id, 'lastName') || ''
-      }`,
+  },
+  {
+    field: 'payment_amount',
+    headerName: 'Payment Amount',
+    width: 160,
   },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  {
+    id: 1,
+    image: 'https://picsum.photos/200',
+    food_name: 'Spagetti Rice',
+    category: 'bread',
+    date: '20/10/2021 8:02 PM',
+    payment_method: 'PayPal',
+    status: 'paid',
+    payment_amount: '#400.00',
+  },
+  {
+    id: 2,
+    image: 'https://picsum.photos/id/237/200',
+    food_name: 'Spagetti Rice',
+    category: 'bread',
+    date: '20/10/2021 8:02 PM',
+    payment_method: 'PayPal',
+    status: 'paid',
+    payment_amount: '#400.00',
+  },
+  {
+    id: 3,
+    image: 'https://picsum.photos/id/1000/200',
+    food_name: 'Spagetti Rice',
+    category: 'bread',
+    date: '20/10/2021 8:02 PM',
+    payment_method: 'PayPal',
+    status: 'paid',
+    payment_amount: '#400.00',
+  },
+  {
+    id: 4,
+    image: 'https://picsum.photos/id/1001/200',
+    food_name: 'Spagetti Rice',
+    category: 'bread',
+    date: '20/10/2021 8:02 PM',
+    payment_method: 'PayPal',
+    status: 'paid',
+    payment_amount: '#400.00',
+  },
 ];
 
 // style const
@@ -123,7 +184,6 @@ export default function Userorders() {
         background: 'white',
         padding: '10px',
         height: 'max-content',
-        //  marginRight: '30px',
       }}
     >
       <Typography>orders</Typography>
